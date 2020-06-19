97bc5468fa98d80c0f2ed1008ae5e5c9e556739a
Guillermo Sesé Santos <guillermosesesantos@hotmail.com>
2020-06-19 23:33:58 +0200

    removed tests, added rate-limit

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 require('dotenv').config();
 
@@ -10,6 +11,12 @@ const api = require('./api');
 
 const app = express();
 
+const limit = rateLimit({
+  windowMs: 2 * 60 * 1000,
+  max: 20,
+});
+
+app.use(limit);
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(cors());
