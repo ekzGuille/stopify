abb539a9df8c9aa1547fa198dadf5b20a1d83539
Guillermo Sesé <guillermosesesantos@hotmail.com>
2020-06-24 01:40:11 +0200

    Loading component

---
+++ client/src/components/loading/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="loading"></div>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+
+  @Component
+  export default class Search extends Vue {
+
+  }
+
+</script>
+
+<style lang="scss">
+  .loading {
+    display: inline-block;
+    border: 4px solid rgb(230, 230, 230);
+    border-left-color: #7983ff;
+    border-radius: 50%;
+    width: 30px;
+    height: 30px;
+    animation: donut-spin 1.2s linear infinite;
+  }
+
+  @keyframes donut-spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  // https://codepen.io/ekzGuille/pen/ExPmeop
+
+</style>
