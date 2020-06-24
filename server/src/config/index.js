d7badc7c73f947950257c4d951bb9eb574b1300a
Guillermo Sesé <guillermosesesantos@hotmail.com>
2020-06-24 14:41:01 +0200

    added .env variables

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,9 +2,10 @@ require('dotenv').config();
 
 /**
  * @typedef ENV
- * @property {string} Var desc
- * @property {string} Var desc
- * @property {string} Var desc
+ * @property {string} SPOTIFY_CLIENT_ID App client id
+ * @property {string} SPOTIFY_CLIENT_ID_SECRET App clien id **SECRET**
+ * @property {string} SPOTIFY_CALLBACK_ENDPOINT Callback endpoint
+ * @property {string} SPOTIFY_DEV_ENDPOINT Spotify developers endpoint
  */
 
 /**
